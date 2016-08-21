45538b5f301773f92618f87f401c5ccdc21e1634
Ben White <benjamin.white1@my.jcu.edu.au>
2016-08-22 09:33:24 +1000

    add modifying lists commands

--- listWarmUp.py
+++ listWarmUp.py
@@ -2,3 +2,14 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
 print (numbers[0], numbers[-7], numbers[3], numbers[:-1], numbers[3:4], 5 in numbers, 7 in numbers, "3" in numbers, numbers + [6, 5, 3])
+
+
+numbers [0] = "ten"
+print(numbers[0])
+
+numbers[-1] = 1
+print(numbers[-1])
+
+print(numbers[2:])
+
+print(9 in numbers)

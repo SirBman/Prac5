48ae64887d482c0a2d942797945ba30f11a63804
Ben White <benjamin.white1@my.jcu.edu.au>
2016-08-22 10:06:30 +1000

    add base list

--- securityChecker.py
+++ securityChecker.py
@@ -2,3 +2,5 @@
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
 'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+

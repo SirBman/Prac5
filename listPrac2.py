41ec484221b5c2030fe8bcb48d87ca5fe44d5b05
Ben White <benjamin.white1@my.jcu.edu.au>
2016-08-22 10:03:05 +1000

    add prac changes

--- listPrac2.py
+++ listPrac2.py
@@ -1,1 +1,18 @@
 """List Practice 2"""
+
+numbers = []
+
+numCount = int(input("How many numbers do you want to list?"))
+for i in range (0,numCount,1):
+    num = int(input("Please enter a number:"))
+    numbers.append(num)
+
+print("The first number is:", numbers[0])
+print("The last number is: ", numbers[-1])
+print("The smalles number is:", min(numbers))
+print("The largest number is:", max(numbers))
+print("The average of the numbers is:", sum(numbers)/len(numbers))
+
+
+
+
